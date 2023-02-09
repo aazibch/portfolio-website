d306f7bae900d129b99c11ace2bbd9469c511c97
Aazib Chaudhry <aazibch47@gmail.com>
2023-02-09 22:59:55 +0500

    Remove 'under construction' message

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import './App.css';
-import UnderConstructionMessage from './components/UnderConstructionMessage/UnderConstructionMessage';
 
 function App() {
-    return (
-        <div className="App">
-            <UnderConstructionMessage />
-        </div>
-    );
+    return <div className="App"></div>;
 }
 
 export default App;
