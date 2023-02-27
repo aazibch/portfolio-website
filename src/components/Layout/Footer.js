da0c0a3796faaf565ada3359b5f75d2f46a0ae0d
Aazib Chaudhry <aazibch47@gmail.com>
2023-02-27 21:02:45 +0500

    Add link to navigation bar

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -2,51 +2,51 @@ import { Container, Link, Typography } from '@mui/material';
 import { Box, Stack } from '@mui/system';
 
 const socialLinks = [
-    {
-        site: 'GitHub',
-        link: 'https://github.com/aazibch/'
-    },
-    {
-        site: 'Twitter',
-        link: 'https://twitter.com/AazibCh'
-    },
-    {
-        site: 'Goodreads',
-        link: 'https://www.goodreads.com/user/show/155259652-aazib-chaudhry'
-    }
+  {
+    site: 'GitHub',
+    link: 'https://github.com/aazibch/'
+  },
+  {
+    site: 'Twitter',
+    link: 'https://twitter.com/AazibCh'
+  },
+  {
+    site: 'Goodreads',
+    link: 'https://www.goodreads.com/user/show/155259652-aazib-chaudhry'
+  }
 ];
 
 const Footer = () => {
-    return (
-        <Container>
-            <Stack
-                sx={{ pt: 20, pb: 10 }}
-                direction={{ xs: 'column', sm: 'row' }}
-                justifyContent="space-between"
-                alignItems={{ xs: 'center', sm: 'end' }}
-                textAlign={{ xs: 'center', sm: 'initial' }}
+  return (
+    <Container>
+      <Stack
+        sx={{ pt: 20, pb: 10 }}
+        direction={{ xs: 'column', sm: 'row' }}
+        justifyContent="space-between"
+        alignItems={{ xs: 'center', sm: 'end' }}
+        textAlign={{ xs: 'center', sm: 'initial' }}
+      >
+        <Box color="text.primary" mb={{ xs: 2, sm: 0 }}>
+          <Typography component="p" variant="h4" mb={1}>
+            Aazib Chaudhry
+          </Typography>
+          <Typography>Copyright © 2023</Typography>
+        </Box>
+        <Box>
+          {socialLinks.map((socialLink) => (
+            <Link
+              key={socialLink.site}
+              mx={1}
+              href={socialLink.link}
+              target="_blank"
             >
-                <Box color="text.primary" mb={{ xs: 2, sm: 0 }}>
-                    <Typography component="p" variant="h4" mb={1}>
-                        Aazib Chaudhry
-                    </Typography>
-                    <Typography>Copyright © 2023</Typography>
-                </Box>
-                <Box>
-                    {socialLinks.map((socialLink) => (
-                        <Link
-                            key={socialLink.site}
-                            mx={1}
-                            href={socialLink.link}
-                            target="_blank"
-                        >
-                            {socialLink.site}
-                        </Link>
-                    ))}
-                </Box>
-            </Stack>
-        </Container>
-    );
+              {socialLink.site}
+            </Link>
+          ))}
+        </Box>
+      </Stack>
+    </Container>
+  );
 };
 
 export default Footer;
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,180 +1,185 @@
 import {
-    AppBar,
-    Box,
-    IconButton,
-    Link,
-    Menu,
-    MenuItem,
-    Switch,
-    Toolbar,
-    Typography
+  AppBar,
+  Box,
+  IconButton,
+  Link,
+  Menu,
+  MenuItem,
+  Switch,
+  Toolbar,
+  Typography
 } from '@mui/material';
 import { Container } from '@mui/system';
 import { Menu as MenuIcon } from '@mui/icons-material';
 import { DarkMode } from '@mui/icons-material';
 import { useState } from 'react';
 
-const sections = [
-    {
-        title: 'About',
-        anchor: '#about'
-    },
-    {
-        title: 'Projects',
-        anchor: '#projects'
-    },
-    {
-        title: 'Contact',
-        anchor: '#contact'
-    }
+const navLinks = [
+  {
+    title: 'About',
+    anchor: '#about'
+  },
+  {
+    title: 'Projects',
+    anchor: '#projects'
+  },
+  {
+    title: 'Contact',
+    anchor: '#contact'
+  },
+  {
+    title: 'Blog',
+    anchor: 'https://blog.aazibch.com/',
+    external: true
+  }
 ];
 
 const Header = (props) => {
-    const [anchorElNav, setAnchorElNav] = useState(null);
+  const [anchorElNav, setAnchorElNav] = useState(null);
 
-    const handleOpenNavMenu = (e) => {
-        setAnchorElNav(e.currentTarget);
-    };
+  const handleOpenNavMenu = (e) => {
+    setAnchorElNav(e.currentTarget);
+  };
 
-    const handleCloseNavMenu = (e) => {
-        setAnchorElNav(null);
-    };
+  const handleCloseNavMenu = (e) => {
+    setAnchorElNav(null);
+  };
 
-    return (
-        <AppBar
-            position="static"
+  return (
+    <AppBar
+      position="static"
+      sx={{
+        backgroundColor: 'background.default',
+        color: 'text.primary',
+        borderColor: 'divider',
+        borderBottomStyle: 'solid',
+        borderWidth: '1px'
+      }}
+      elevation={0}
+    >
+      <Container maxWidth="lg">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h5"
+            component="a"
+            href="#hero"
             sx={{
-                backgroundColor: 'background.default',
-                color: 'text.primary',
-                borderColor: 'divider',
-                borderBottomStyle: 'solid',
-                borderWidth: '1px'
+              textDecoration: 'none',
+              color: 'inherit',
+              mr: 2,
+              display: { xs: 'none', md: 'flex' }
             }}
-            elevation={0}
-        >
-            <Container maxWidth="lg">
-                <Toolbar disableGutters>
-                    <Typography
-                        variant="h5"
-                        component="a"
-                        href="#hero"
-                        sx={{
-                            textDecoration: 'none',
-                            color: 'inherit',
-                            mr: 2,
-                            display: { xs: 'none', md: 'flex' }
-                        }}
-                    >
-                        Aazib Chaudhry
-                    </Typography>
-                    <Box
-                        sx={{
-                            flexGrow: 1,
-                            display: { xs: 'flex', md: 'none' }
-                        }}
-                    >
-                        <IconButton
-                            size="large"
-                            aria-label="Menu button"
-                            aria-controls="menu-appbar"
-                            aria-haspopup="true"
-                            color="inherit"
-                            onClick={handleOpenNavMenu}
-                        >
-                            <MenuIcon />
-                        </IconButton>
-                        <Menu
-                            open={Boolean(anchorElNav)}
-                            id="menu-appbar"
-                            anchorEl={anchorElNav}
-                            anchorOrigin={{
-                                vertical: 'bottom',
-                                horizontal: 'left'
-                            }}
-                            onClose={handleCloseNavMenu}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'left'
-                            }}
-                            sx={{
-                                display: { xs: 'block', md: 'none' }
-                            }}
-                        >
-                            {sections.map((section) => (
-                                <MenuItem
-                                    component="a"
-                                    href={section.anchor}
-                                    key={section.title}
-                                    sx={{ textTransform: 'none' }}
-                                    onClick={handleCloseNavMenu}
-                                >
-                                    <Typography textAlign="center">
-                                        {section.title}
-                                    </Typography>
-                                </MenuItem>
-                            ))}
-                        </Menu>
-                    </Box>
-                    <Typography
-                        variant="h5"
-                        component="a"
-                        href="#hero"
-                        sx={{
-                            textDecoration: 'none',
-                            color: 'inherit',
-                            flexGrow: 1,
-                            mr: 2,
-                            fontWeight: 400,
-                            display: { xs: 'flex', md: 'none' }
-                        }}
-                    >
-                        Aazib Chaudhry
-                    </Typography>
+          >
+            Aazib Chaudhry
+          </Typography>
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: 'flex', md: 'none' }
+            }}
+          >
+            <IconButton
+              size="large"
+              aria-label="Menu button"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              color="inherit"
+              onClick={handleOpenNavMenu}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              open={Boolean(anchorElNav)}
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left'
+              }}
+              onClose={handleCloseNavMenu}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left'
+              }}
+              sx={{
+                display: { xs: 'block', md: 'none' }
+              }}
+            >
+              {navLinks.map((navLink) => (
+                <MenuItem
+                  component="a"
+                  href={navLink.anchor}
+                  key={navLink.title}
+                  sx={{ textTransform: 'none' }}
+                  onClick={handleCloseNavMenu}
+                  target={navLink.external ? '_blank' : '_self'}
+                >
+                  <Typography textAlign="center">{navLink.title}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Typography
+            variant="h5"
+            component="a"
+            href="#hero"
+            sx={{
+              textDecoration: 'none',
+              color: 'inherit',
+              flexGrow: 1,
+              mr: 2,
+              fontWeight: 400,
+              display: { xs: 'flex', md: 'none' }
+            }}
+          >
+            Aazib Chaudhry
+          </Typography>
 
-                    <Box
-                        sx={{
-                            flexGrow: 1,
-                            display: { xs: 'none', md: 'flex' }
-                        }}
-                    >
-                        {sections.map((section) => {
-                            return (
-                                <Link
-                                    underline="none"
-                                    href={section.anchor}
-                                    key={section.title}
-                                    sx={{
-                                        color: 'inherit',
-                                        textTransform: 'none',
-                                        fontWeight: 400,
-                                        m: 1
-                                    }}
-                                >
-                                    {section.title}
-                                </Link>
-                            );
-                        })}
-                    </Box>
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: 'none', md: 'flex' }
+            }}
+          >
+            {navLinks.map((navLink) => {
+              return (
+                <Link
+                  underline="none"
+                  href={navLink.anchor}
+                  target={navLink.external ? '_blank' : '_self'}
+                  key={navLink.title}
+                  sx={{
+                    color: 'inherit',
+                    textTransform: 'none',
+                    fontWeight: 400,
+                    m: 1
+                  }}
+                >
+                  {navLink.title}
+                </Link>
+              );
+            })}
+          </Box>
 
-                    <Box
-                        sx={{
-                            flexGrow: 0,
-                            display: 'flex',
-                            alignItems: 'center',
-                            justifyContent: 'center'
-                        }}
-                    >
-                        <DarkMode sx={{ fontSize: 30, color: 'grey' }} />
-                        <Switch
-                            checked={props.themeMode === 'dark'}
-                            onChange={props.handleThemeMode}
-                        />
-                    </Box>
-                </Toolbar>
-            </Container>
-        </AppBar>
-    );
+          <Box
+            sx={{
+              flexGrow: 0,
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center'
+            }}
+          >
+            <DarkMode sx={{ fontSize: 30, color: 'grey' }} />
+            <Switch
+              checked={props.themeMode === 'dark'}
+              onChange={props.handleThemeMode}
+            />
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 };
 
 export default Header;
