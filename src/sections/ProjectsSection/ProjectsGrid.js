8048f5bdc2094c11611eda6c92fc8c663f0222ef
Aazib Chaudhry <aazibch47@gmail.com>
2023-04-17 02:52:29 +0500

    Add project

--- src/sections/ProjectsSection/ProjectsGrid.js
+++ src/sections/ProjectsSection/ProjectsGrid.js
@@ -1,86 +1,85 @@
 import { Grid } from '@mui/material';
 import Project from './Project';
-import MyNotesScreen from './screenshots/mynotes.png';
 import JokeSlidesScreen from './screenshots/jokeslides.png';
 import ReadsTrackerScreen from './screenshots/readstracker.png';
 import PoemPadScreen from './screenshots/poempad.png';
 import SimonScreen from './screenshots/simon.png';
-import PomodoroScreen from './screenshots/pomodoroclock.png';
+import VirtuaVisageScreen from './screenshots/virtuavisage.png';
+import CummingsBreitenbergScreen from './screenshots/cummingsbreitenberg.png';
 
 const projects = [
-    {
-        title: 'MyNotes',
-        tech: ['React', 'Firebase'],
-        image: MyNotesScreen,
-        links: {
-            demo: 'https://aazibch.github.io/mynotes/',
-            repo: 'https://github.com/aazibch/mynotes'
-        }
-    },
-    {
-        title: 'JokeSlides',
-        tech: ['React', 'Express', 'MongoDB'],
-        image: JokeSlidesScreen,
-        links: {
-            demo: 'https://jokeslides.up.railway.app/',
-            repo: 'https://github.com/aazibch/jokeslides'
-        }
-    },
-    {
-        title: 'ReadsTracker',
-        tech: ['Pug', 'Express', 'MongoDB'],
-        image: ReadsTrackerScreen,
-        links: {
-            demo: 'https://readstracker.up.railway.app/',
-            repo: 'https://github.com/aazibch/readstracker'
-        }
-    },
-    {
-        title: 'PoemPad',
-        tech: ['HTML', 'SCSS', 'JavaScript', 'Webpack'],
-        image: PoemPadScreen,
-        links: {
-            demo: 'https://poempad.netlify.app/',
-            repo: 'https://github.com/aazibch/poempad'
-        }
-    },
-    {
-        title: 'Simon Game',
-        tech: ['HTML', 'CSS', 'JavaScript'],
-        image: SimonScreen,
-        links: {
-            demo: 'https://simon-production.netlify.app/',
-            repo: 'https://github.com/aazibch/simon'
-        }
-    },
-    {
-        title: 'Pomodoro Clock',
-        tech: ['HTML', 'CSS', 'JavaScript'],
-        image: PomodoroScreen,
-        links: {
-            demo: 'https://pomodoro-production.netlify.app/',
-            repo: 'https://github.com/aazibch/pomodoro-clock'
-        }
+  {
+    title: 'VirtuaVisage',
+    tech: ['React', 'Express', 'MongoDB'],
+    image: VirtuaVisageScreen,
+    links: {
+      demo: 'https://virtuavisage.onrender.com/',
+      repo: 'https://github.com/aazibch/virtuavisage'
     }
+  },
+  {
+    title: 'Cummings-Breitenberg LLC',
+    tech: ['Next'],
+    image: CummingsBreitenbergScreen,
+    links: {
+      demo: 'https://cbllc.netlify.app/'
+    }
+  },
+  {
+    title: 'JokeSlides',
+    tech: ['React', 'Express', 'MongoDB'],
+    image: JokeSlidesScreen,
+    links: {
+      demo: 'https://jokeslides.up.railway.app/',
+      repo: 'https://github.com/aazibch/jokeslides'
+    }
+  },
+  {
+    title: 'ReadsTracker',
+    tech: ['Pug', 'Express', 'MongoDB'],
+    image: ReadsTrackerScreen,
+    links: {
+      demo: 'https://readstracker.up.railway.app/',
+      repo: 'https://github.com/aazibch/readstracker'
+    }
+  },
+  {
+    title: 'PoemPad',
+    tech: ['HTML', 'SCSS', 'JavaScript', 'Webpack'],
+    image: PoemPadScreen,
+    links: {
+      demo: 'https://poempad.netlify.app/',
+      repo: 'https://github.com/aazibch/poempad'
+    }
+  },
+  {
+    title: 'Simon Game',
+    tech: ['HTML', 'CSS', 'JavaScript'],
+    image: SimonScreen,
+    links: {
+      demo: 'https://simon-production.netlify.app/',
+      repo: 'https://github.com/aazibch/simon'
+    }
+  }
 ];
 
 const ProjectsGrid = () => {
-    return (
-        <Grid container spacing={2}>
-            {projects.map((project) => {
-                return (
-                    <Grid key={project.title} item xs={12} sm={6} md={4}>
-                        <Project
-                            title={project.title}
-                            tech={project.tech}
-                            image={project.image}
-                            links={project.links}
-                        />
-                    </Grid>
-                );
-            })}
-        </Grid>
-    );
+  return (
+    <Grid container spacing={2}>
+      {projects.map((project) => {
+        return (
+          <Grid key={project.title} item xs={12} sm={6} md={4}>
+            <Project
+              title={project.title}
+              tech={project.tech}
+              image={project.image}
+              links={project.links}
+            />
+          </Grid>
+        );
+      })}
+    </Grid>
+  );
 };
 
 export default ProjectsGrid;
