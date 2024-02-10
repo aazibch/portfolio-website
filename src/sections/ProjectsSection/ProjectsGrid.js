7f071e9df82566bbf4e852fbcdafbb6ab32a618f
Aazib Chaudhry <aazibch47@gmail.com>
2024-02-10 17:43:46 +0500

    Add new project

--- src/sections/ProjectsSection/ProjectsGrid.js
+++ src/sections/ProjectsSection/ProjectsGrid.js
@@ -1,6 +1,5 @@
 import { Grid } from '@mui/material';
 import Project from './Project';
-import JokeSlidesScreen from './screenshots/jokeslides.png';
 import ReadsTrackerScreen from './screenshots/readstracker.png';
 import SimonScreen from './screenshots/simon.png';
 import YumYumScreen from './screenshots/yumyum.png';
@@ -9,15 +8,25 @@ import CummingsBreitenbergScreen from './screenshots/cummingsbreitenberg.png';
 import WebsoftScreen from './screenshots/websoft.png';
 import ExpenseTrackerScreen from './screenshots/expensetracker.png';
 import ZephyrMessengerScreen from './screenshots/zephyrmessenger.png';
+import GreenEatsScreen from './screenshots/greeneats.png';
 
 const projects = [
+  {
+    title: 'GreenEats',
+    tech: ['Next', 'SQLite', 'AWS S3'],
+    image: GreenEatsScreen,
+    links: {
+      demo: 'https://greeneats-demo.netlify.app/',
+      repo: 'https://github.com/aazibch/greeneats/'
+    }
+  },
   {
     title: 'ZephyrMessenger',
     tech: ['React', 'React Router 6', 'Express', 'MongoDB'],
     image: ZephyrMessengerScreen,
     links: {
       demo: 'https://zephyr-messenger.onrender.com/',
-      repo: 'https://github.com/aazibch/zephyr-messenger'
+      repo: 'https://github.com/aazibch/zephyr-messenger/'
     }
   },
   {
@@ -26,7 +35,7 @@ const projects = [
     image: VirtuaVisageScreen,
     links: {
       demo: 'https://virtuavisage.onrender.com/',
-      repo: 'https://github.com/aazibch/virtuavisage'
+      repo: 'https://github.com/aazibch/virtuavisage/'
     }
   },
   {
@@ -38,21 +47,12 @@ const projects = [
     }
   },
   {
-    title: 'JokeSlides',
-    tech: ['React', 'Express', 'MongoDB'],
-    image: JokeSlidesScreen,
-    links: {
-      demo: 'https://jokeslides.onrender.com/',
-      repo: 'https://github.com/aazibch/jokeslides'
-    }
-  },
-  {
     title: 'ReadsTracker',
     tech: ['Pug', 'Express', 'MongoDB'],
     image: ReadsTrackerScreen,
     links: {
       demo: 'https://readstracker.onrender.com/',
-      repo: 'https://github.com/aazibch/readstracker'
+      repo: 'https://github.com/aazibch/readstracker/'
     }
   },
   {
@@ -61,7 +61,7 @@ const projects = [
     image: ExpenseTrackerScreen,
     links: {
       demo: 'https://expense-tracker-production.netlify.app/',
-      repo: 'https://github.com/aazibch/expense-tracker'
+      repo: 'https://github.com/aazibch/expense-tracker/'
     }
   },
   {
@@ -79,7 +79,7 @@ const projects = [
     image: SimonScreen,
     links: {
       demo: 'https://simon-production.netlify.app/',
-      repo: 'https://github.com/aazibch/simon'
+      repo: 'https://github.com/aazibch/simon/'
     }
   },
   {
@@ -88,7 +88,7 @@ const projects = [
     image: WebsoftScreen,
     links: {
       demo: 'https://websoft.onrender.com/',
-      repo: 'https://github.com/aazibch/websoft'
+      repo: 'https://github.com/aazibch/websoft/'
     }
   }
 ];
