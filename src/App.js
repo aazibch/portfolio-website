e70c954bfdf9ceb159fa86a1bae0e32d713c72e2
Aazib Chaudhry <aazibch47@gmail.com>
2026-09-03 09:43:25 +0500

    Set default color scheme based on user's system-wide preferences

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { createTheme, ThemeProvider } from '@mui/material';
+import { createTheme, ThemeProvider, useMediaQuery } from '@mui/material';
 import { Box } from '@mui/system';
 import { useEffect, useState } from 'react';
 import './App.css';
@@ -7,50 +7,53 @@ import Sections from './components/Sections/Sections';
 import getThemeConfig from './theme';
 
 function App() {
-    const [themeMode, setThemeMode] = useState('light');
-    const [firstRun, setFirstRun] = useState(true);
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const [themeMode, setThemeMode] = useState(
+    prefersDarkMode ? 'dark' : 'light'
+  );
+  const [firstRun, setFirstRun] = useState(true);
 
-    useEffect(() => {
-        if (firstRun) {
-            const theme = localStorage.getItem('themeMode');
+  useEffect(() => {
+    if (firstRun) {
+      const theme = localStorage.getItem('themeMode');
 
-            if (!theme) {
-                localStorage.setItem('themeMode', themeMode);
-            } else {
-                setThemeMode(theme);
-            }
+      if (!theme) {
+        localStorage.setItem('themeMode', themeMode);
+      } else {
+        setThemeMode(theme);
+      }
 
-            setFirstRun(false);
-        }
+      setFirstRun(false);
+    }
 
-        if (!firstRun) {
-            localStorage.setItem('themeMode', themeMode);
-        }
-    }, [themeMode, firstRun]);
+    if (!firstRun) {
+      localStorage.setItem('themeMode', themeMode);
+    }
+  }, [themeMode, firstRun]);
 
-    const siteTheme = createTheme(getThemeConfig(themeMode));
+  const siteTheme = createTheme(getThemeConfig(themeMode));
 
-    const handleThemeMode = () => {
-        setThemeMode((prevThemeMode) => {
-            if (prevThemeMode === 'dark') {
-                return 'light';
-            }
+  const handleThemeMode = () => {
+    setThemeMode((prevThemeMode) => {
+      if (prevThemeMode === 'dark') {
+        return 'light';
+      }
 
-            if (prevThemeMode === 'light') {
-                return 'dark';
-            }
-        });
-    };
+      if (prevThemeMode === 'light') {
+        return 'dark';
+      }
+    });
+  };
 
-    return (
-        <ThemeProvider theme={siteTheme}>
-            <Box bgcolor="background.default">
-                <Layout handleThemeMode={handleThemeMode} themeMode={themeMode}>
-                    <Sections />
-                </Layout>
-            </Box>
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider theme={siteTheme}>
+      <Box bgcolor="background.default">
+        <Layout handleThemeMode={handleThemeMode} themeMode={themeMode}>
+          <Sections />
+        </Layout>
+      </Box>
+    </ThemeProvider>
+  );
 }
 
 export default App;
